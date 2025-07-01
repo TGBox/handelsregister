89e3a5f00288854ac54ee5f0ec979c5b74efbc5e
DanielRoesch <daniel.roesch@immersight.de>
2025-07-01 15:39:22 +0200

    add new pysel.py file that is a new implementation of the selenium-cli.py from imsMailVerify

---
+++ hr/pysel.py
@@ -0,0 +1,363 @@
+# Selenium/Python powered stand-alone module to provide convenient programmatic access the bundesAPI WebSearch.
+import sys
+from hr.pyutil import extract_company_address, extract_company_name, extract_management_data
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import time
+import os
+import argparse
+
+# ! Second adaptation of selenium_handelsregister.py - WIP
+# Contains the updated versions of the extraction methods that have been introduced via pyutil.py from imsMailVerify.
+# Needs to get called with the keyword argument syntax. This pairs each value to a specific key, which eleminates the need for correct order of params.
+
+def parse_cli_arguments():
+    """
+        Function to parse the arguments that were passed on to this python script when it was executed.
+        
+        Returns:
+            Dictionary containing all key=value pairs.
+    """
+    # Creating a parser with name, description and help text.
+    parser = argparse.ArgumentParser(
+        prog="Selenium - Python | HandelsregisterCLI",
+        description="Ein Python Skript, welches mit Selenium die Suche von Einträgen im Handelsregister automatisieren kann.",
+        add_help=True,
+        epilog="Achtung! Maximal 60 Anfragen pro Stunde stellen!"
+    )
+
+    # Adding help texts for our expected arguments. NOT NEEDED ANYMORE!
+    #parser.add_argument("searchTerms", nargs='+', help="Eine Liste von zu verarbeitenden Strings. 1. String ist der eigentliche Suchstring. 2. String zeigt an, welche Suchoptionen gewählt werden sollen. (all, exact oder min) 3. String ist der Name der Stadt. 4. String ist die Straße (und ggf. die Hausnummer). Der 5. String repräsentiert die Postleitzahl. Der 6. String entscheidet mit einem Boolean Wert, ob auch phonetisch ähnlich klingende Worte gesucht werden sollen. (Default false)")
+
+    # Parsing the arguments from the command line.
+    parser.add_argument(
+        "-d",
+        "--debug",
+        help="Enable debug mode and activate logging",
+        action="store_true"
+    )
+    parser.add_argument(
+        "-f",
+        "--force",
+        help="Force a fresh pull and skip the cache",
+        action="store_true"
+    )
+    parser.add_argument(
+        "-s",
+        "--schlagwoerter",
+        help="Search for the provided keywords",
+        required=True,
+    )
+    parser.add_argument(
+        "-so",
+        "--schlagwortOptionen",
+        help="Keyword options: all=contain all keywords; min=contain at least one keyword; exact=contain the exact company name.",
+        choices=["all", "min", "exact"],
+        default="all"
+    )
+    parser.add_argument(
+        "-sa",
+        "-sae",
+        "--sucheAehnliche",
+        help="Should phonetically similar sounding results also get returned?",
+        action="store_true",
+        required=False,
+        default=False
+    )
+    parser.add_argument(
+        "-sg",
+        "-sge",
+        "-sog",
+        "--sucheGeloeschte",
+        help="Should already removed results get returned as well?",
+        action="store_true",
+        required=False,
+        default=False
+    )
+    parser.add_argument(
+        "-n",
+        "--registerNummer",
+        help="Add registry number to improve search results",
+        required=False
+    )
+    parser.add_argument(
+        "-ci",
+        "--city",
+        help="Name of the city where the company is located.",
+        required=False
+    )
+    parser.add_argument(
+        "-st",
+        "--street",
+        help="Name of the street where the company is located.",
+        required=False
+    )
+    parser.add_argument(
+        "-po",
+        "--postCode",
+        help="Post code of the city where the company is located.",
+        required=False
+    )
+    args = parser.parse_args()
+
+    # Enable debugging if wanted
+    if args.debug:
+        import logging
+        logger = logging.getLogger("mechanize")
+        logger.addHandler(logging.StreamHandler(sys.stdout))
+        logger.setLevel(logging.DEBUG)
+
+    return args
+
+def fetch_and_download_from_bundes_api(s, so, sa, sg, ci, st, po):
+    """Function to fetch and download a specific data request/response from the handelsregister bundesAPI.
+    Currently only decorative for checking on the way that the input gets passed along.
+
+    Args:
+        s (str): the search term (i.e. name of the company)
+        so (str): search options - "all", "exact" or "min"
+        sa (bool): if phonetically similar sounding results should get returned, too.
+        sg (bool): if already deleted entries should get returned, too.
+        ci (str): the name of the city
+        st (str): the name of the street (and possibly the house number)
+        po (str): the post code of the city
+    """
+    # todo: replace with best practice alternative.
+    # os.getcwd() refers to the active directory in windows. (= our root folder in the project.) => Modified to give each request its own download folder.
+    dl_path = os.path.join(os.getcwd(), "download", s)
+    if not os.path.isdir(dl_path): # Creating the folder; but only if it does not exist yet.
+        os.makedirs(dl_path)
+
+    # Chrome Options.
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')  # Browser im Hintergrund ohne UI ausführen
+    chrome_options.add_argument('--disable-gpu') # Manchmal nötig
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--window-size=1920,1080')
+    # Chrome options setup to ensure that we can download and that we know where the file will get downloaded to.
+    chrome_options.add_experimental_option('prefs', {
+        'download.default_directory': dl_path,  
+        'download.prompt_for_download': False,
+        'download.directory_upgrade': True,
+        'safebrowsing.enabled': True,
+        "profile.default_content_settings.popups": 0,
+        "profile.content_settings.exceptions.automatic_downloads.*.setting": 1,
+        "profile.default_content_setting_values.automatic_downloads": 1,
+        "profile.default_content_settings.mimetype_overrides": {
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        }
+    })
+
+    try:
+        # Webdriver-manager loads the appropriate driver or uses a cached one.
+        service = ChromeService(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+        print("WebDriver erfolgreich mit webdriver-manager initialisiert.")
+    except Exception as e:
+        print(f"Fehler beim Initialisieren des WebDrivers mit webdriver-manager: {e}")
+        exit()
+
+    try:
+        # Trying to get the elements via their IDs.
+        driver.get("https://www.handelsregister.de/rp_web/welcome.xhtml")
+        target_class_selector = "a.ui-commandlink.ui-widget.dokumentList"
+        advanced_search = "naviForm:erweiterteSucheLink"
+        search_terms = "form:schlagwoerter"
+        
+        search_options_all = "form:schlagwortOptionen:0"
+        search_options_exact = "form:schlagwortOptionen:1"
+        search_options_min = "form:schlagwortOptionen:2"
+        
+        search_options = search_options_all  # Default value to avoid unbound error
+        if so == "all":
+            search_options = search_options_all
+        elif so == "exact":
+            search_options = search_options_exact
+        elif so == "min":
+            search_options = search_options_min
+        
+        post_code = "form:postleitzahl"
+        city = "form:ort"
+        street = "form:strasse"
+        submitBtn = "form:btnSuche"
+        
+    ######## Interaction with the elements inside of the webpage search form. #########
+    # Change to the advanced search form.    
+        wait = WebDriverWait(driver, 10)
+        try:
+            search_link = wait.until(EC.element_to_be_clickable((By.ID, advanced_search)))
+            search_link.click()
+        except TimeoutException:
+            print("Es wurde nicht rechtzeitig ein Button für die Erweiterte Suche gefunden.")
+            search_link = ""
+            
+        print(f"Wechsel zur erweiterten Suche und Eintragen der Parameter...")
+        # Changed to the page containing the search form.
+    # Click on textbox and enter search term.
+        wait = WebDriverWait(driver, 10)
+        try:
+            text_box = wait.until(EC.element_to_be_clickable((By.ID, search_terms)))
+            text_box.send_keys(s)
+        except TimeoutException:
+            print("Es wurde nicht rechtzeitig eine Textbox für die Eingabe von Suchbegriffen gefunden.")
+            text_box = ""
+        
+    # Find radio button label that corresponds to the selected option and click it.
+        wait = WebDriverWait(driver, 10)
+        try:
+            radioBtnLabel = wait.until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, f"label[for='{search_options}']"))
+            )
+            radioBtnLabel.click()
+            time.sleep(2)
+        except TimeoutException:
+            print("Es wurde nicht rechtzeitig ein klickbarer Radiobutton für die Eingabe von Suchoptionen gefunden.")
+            radioBtnLabel = ""
+
+    # Find the checkbox for similar sounding search results getting fetched as well.
+        wait = WebDriverWait(driver, 10)
+        try:
+            checkbox_input = driver.find_element(By.ID, "form:aenlichLautendeSchlagwoerterBoolChkbox_input")
+            if checkbox_input.is_selected():
+                checkbox_container = wait.until(EC.element_to_be_clickable((By.ID, "form:aenlichLautendeSchlagwoerterBoolChkbox")))
+                if (sa == False):
+                    checkbox_container.click()  # deselect already selected if we do not want to search for similar!
+            else:
+                checkbox_container = wait.until(EC.element_to_be_clickable((By.ID, "form:aenlichLautendeSchlagwoerterBoolChkbox")))
+                if (sa == True):
+                    checkbox_container.click() # select deselected if we want to search for similar!
+            time.sleep(2)
+        except TimeoutException:
+            print("Es wurde nicht rechtzeitig eine klickbare Checkbox für die Auswahl zur Suche von ähnlich klingenden Einträgen gefunden.")
+            checkbox_container = ""
+            
+
+
+    # Find text input for the post code and enter it.
+        wait = WebDriverWait(driver, 10)
+        try:
+            plz = wait.until(EC.element_to_be_clickable((By.ID, post_code)))
+            plz.send_keys(po)
+        except TimeoutException:
+            print("Es wurde nicht rechtzeitig eine Textbox für die Postleitzahleneingabe gefunden.")
+            plz = ""
+            
+    # Find text input for the city name and enter it.
+        wait = WebDriverWait(driver, 10)
+        try:
+            ort = wait.until(EC.element_to_be_clickable((By.ID, city)))
+            ort.send_keys(ci)
+        except TimeoutException:
+            print("Es wurde nicht rechtzeitig eine Textbox für die Ortseingabe gefunden.")
+            ort = ""
+            
+    # Find text input for the street name and 
+        wait = WebDriverWait(driver, 10)
+        try:
+            strt = wait.until(EC.element_to_be_clickable((By.ID, street)))
+            strt.send_keys(st)
+        except TimeoutException:
+            print("Es wurde nicht rechtzeitig eine Textbox für die Straßennameneingabe gefunden.")
+            strt = ""
+        
+
+        wait = WebDriverWait(driver, 10)
+        try:
+            subBtn = wait.until(EC.element_to_be_clickable((By.ID, submitBtn)))
+            subBtn.click()
+            
+            wait = WebDriverWait(driver, 10)
+            
+        except TimeoutException:
+            print("Es wurde nicht rechtzeitig ein Submitbutton gefunden.")
+            subBtn = ""
+        
+        # Es ist wichtig, auf die Sichtbarkeit/Klickbarkeit der Elemente zu warten,
+        # besonders bei dynamischen Webseiten.
+        wait = WebDriverWait(driver, 20) # Maximal 20 Sekunden warten
+        
+        try:
+            links = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, target_class_selector)))
+        except TimeoutException:
+            print("Keine Links mit der Klasse gefunden oder nicht rechtzeitig geladen.")
+            links = []
+
+        print(f"Anzahl gefundener Links: {len(links)}")
+
+        link_id = None  # Ensure link_id is always defined
+        for i, link_element in enumerate(links):
+            try:
+                link_text = link_element.text
+                link_id = link_element.get_attribute("id")
+                print(f"Link {i+1}: Text='{link_text}', ID='{link_id}'")
+
+                # We don't need the document overview, the newest publications or the corporate sponsors. We only really need the current status.
+                if (link_text != "DK") and (link_text != "VÖ") and (link_text != "UT") and (link_text != "SI") and (link_text != "CD") and (link_text != "HD"):
+                    print(f"Versuche Link mit Text '{link_text}' und ID '{link_id}' zu klicken...")
+
+                    # Stelle sicher, dass das Element klickbar ist
+                    if link_id is not None:
+                        wait.until(EC.element_to_be_clickable((By.ID, link_id)))
+                        link_element.click()
+                        print("Link geklickt!")
+                    else:
+                        print("Warnung: link_id ist None, überspringe Klick.")
+
+                    time.sleep(1)
+                    break
+                    
+            except Exception as e:
+                print(f"Fehler beim Verarbeiten oder Klicken von Link {link_id if link_id is not None else ' unbekannt'}: {e}")
+
+    except Exception as e:
+        print(f"Ein genereller Fehler ist aufgetreten: {e}")
+
+    finally:
+        # ! Wenn die Zeile unter dieser nicht auskommentiert ist, dann muss der Browser manuell geschlossen werden.
+        #input("Drücke Enter, um den Browser zu schließen...") # Zum Debuggen
+        
+        # todo: replace with pathlib best practice alternative! https://realpython.com/python-pathlib/ 
+        if 'driver' in locals():
+            driver.quit()
+        if os.path.exists("temp_page.html"):
+            os.remove("temp_page.html")
+        
+        print("Daten wurden heruntergeladen. Extrahieren der Geschäftsführer und Prokuristen wird gestartet...")
+        # Retrieving the filepath of the downloaded document and parsing it. 
+        pdfFileName = os.listdir(dl_path)[0]
+        pdfFilePath = os.path.join(dl_path, pdfFileName)
+        managers = extract_management_data(pdfFilePath)
+        companyName = extract_company_name(pdfFilePath)
+        companyAddress = extract_company_address(pdfFilePath)
+
+        # Iteration over the resulting values to print them to console.
+        if managers:  # Only take lists that are non-empty.
+            print("Gefundene Personen in dieser Liste:")
+            for person in managers:
+                print(f"  - {person}")
+                    
+        if companyName:            
+            print("Name der Firma: " + companyName)
+        if companyAddress:
+            print("Addresse der Firma: " + companyAddress)
+            
+        
+if __name__ == "__main__":
+    print("PySel wird ausgeführt...")
+    print("Wurde gecalled mit folgenden Parametern:")
+    args = parse_cli_arguments()
+    print(args)
+    fetch_and_download_from_bundes_api(
+        args.schlagwoerter,
+        args.schlagwortOptionen,
+        args.sucheAehnliche,
+        args.sucheGeloeschte,
+        args.city,
+        args.street,
+        args.postCode
+    )
